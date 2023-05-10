a19e0b7086362f5091ad27fe614e8ce2468240e1
Chinathip Chaithaworn <singto243@gmail.com>
2023-05-10 19:00:06 +0700

    Update Login Logo

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -12,22 +12,6 @@ export default function SignIn()  {
             mem_password: data.get('password'),
         }
 
-        console.log(jsonData)
-
-        // fetch("http://localhost:3333/login", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify(jsonData),
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log("Success:", data);
-        // })
-        // .catch ((error) => {
-        //     console.error("Error:", error);
-        // });
         async function postJSON(data) {
             try {
                 const response = await fetch("http://localhost:3333/login", {
@@ -61,52 +45,55 @@ export default function SignIn()  {
     }, []);
 
     return (
-        <div className="loginFormContainer">
-            <form className="login_form" onSubmit={handleSubmit}>
-                <div className='loginForm'>
-                    <div className='lBUser'>
-                        <label className="labelUsername">ชื่อผู้ใช้งาน</label>
-                        <label className="linkSignUp">ยังไม่มีบัญชี <Link to='/SignUp'>สมัครสมาชิก</Link></label>
-                    </div>
-                    <div className='inputUsername'>
-                        <input className="inputTextUsername"
-                        name="username"
-                        type="text"
-                        placeholder="กรอกชื่อผู้ใช้งาน"
-                        // value={username}
-                        // onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <br />
-                    <div className='lBPwd'>
-                        <label className="labelPassword">รหัสผ่าน</label>
-                    </div>
-                    <div className='inputPass'>
-                        <input className="inputTextPass"
-                        name="password"
-                        type="password"
-                        placeholder="กรอกรหัสผ่าน"
-                        // value={password}
-                        // onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className='frameBtSignIn'>
-                        <button className="btSignIn" 
-                        type="submit"
-                        variant="contained"
-                        >เข้าสู่ระบบ</button>
-                    </div>
-                    <div className='xt_1_hr'>
-                        <hr className='line'></hr>
-                            <div className="label1">เข้าใช้งานผ่าน</div>
-                        <hr className='line'></hr>
-                    </div>
-                    <div className='lg_google'>
-                        <button className='bt_lwg' type="button"><FcGoogle className="iconGoogle"/>เข้าสู่ระบบผ่าน google</button>
-                    </div>
-                <p className='plc_pvr'>การดำเนินการต่อแสดงว่าคุณยอมรับข้อกำหนดในการให้บริการของ <Link to="https://chat.openai.com/" target="_blank" >Kabixoo</Link> และ <Link to=" ">นโยบายความเป็นส่วนตัว</Link></p>
+        <div>
+            <div className="loginFormContainer">
+                <div className='logoContainer'>
+                    <img src={require('../images/login_logo.png')} className='logoSize'></img>
                 </div>
-            </form>
+                <form className="login_form" onSubmit={handleSubmit}>
+                    <div className='loginForm'>
+                        <div className='lBUser'>
+                            <label className="labelUsername">ชื่อผู้ใช้งาน</label>
+                            <label className="linkSignUp">ยังไม่มีบัญชี <Link to='/SignUp'>สมัครสมาชิก</Link></label>
+                        </div>
+                        <div className='inputUsername'>
+                            <input className="inputTextUsername"
+                            name="username"
+                            type="text"
+                            placeholder="กรอกชื่อผู้ใช้งาน"
+                            />
+                        </div>
+                        <br />
+                        <div className='lBPwd'>
+                            <label className="labelPassword">รหัสผ่าน</label>
+                        </div>
+                        <div className='inputPass'>
+                            <input className="inputTextPass"
+                            name="password"
+                            type="password"
+                            placeholder="กรอกรหัสผ่าน"
+                            // value={password}
+                            // onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className='frameBtSignIn'>
+                            <button className="btSignIn" 
+                            type="submit"
+                            variant="contained"
+                            >เข้าสู่ระบบ</button>
+                        </div>
+                        <div className='xt_1_hr'>
+                            <hr className='line'></hr>
+                                <div className="label1">เข้าใช้งานผ่าน</div>
+                            <hr className='line'></hr>
+                        </div>
+                        <div className='lg_google'>
+                            <button className='bt_lwg' type="button"><FcGoogle className="iconGoogle"/>เข้าสู่ระบบผ่าน google</button>
+                        </div>
+                    <p className='plc_pvr'>การดำเนินการต่อแสดงว่าคุณยอมรับข้อกำหนดในการให้บริการของ <Link to="https://chat.openai.com/" target="_blank" >Kabixoo</Link> และ <Link to=" ">นโยบายความเป็นส่วนตัว</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
