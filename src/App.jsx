d672d49cf43a0370d1edf3977cb84ae0fd68782f
singto243 <89060995+singto243@users.noreply.github.com>
2023-05-25 20:22:54 +0700

    Create Article Page with Responsive All Device and fix navbar and Review Page a little bit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter , Route, Routes, useLocation  } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import ArticlePage from './pages/ArticlePage';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Review from './pages/Review';
+import Article from './pages/Article';
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar/>}>
-            <Route index  element={<Review />}/>
-            <Route path="article" element={<ArticlePage />}/>
+            <Route index  element={<Home />}/>
+            <Route path="article" element={<Article />}/>
             <Route path="about" element={<About />}/>
             <Route path="signin" element={<SignIn />}/>
             <Route path="signup" element={<SignUp />}/>
